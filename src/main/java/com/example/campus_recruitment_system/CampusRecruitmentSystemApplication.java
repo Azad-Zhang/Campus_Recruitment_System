66defe5397b61872da5177afedb85fc8640247d2
Zhangshipeng <1120148291@qq.com>
2022-04-04 10:46:58 +0800

    '初始化'

---
+++ src/main/java/com/example/campus_recruitment_system/CampusRecruitmentSystemApplication.java
@@ -0,0 +1,13 @@
+package com.example.campus_recruitment_system;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CampusRecruitmentSystemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CampusRecruitmentSystemApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/campus_recruitment_system/CampusRecruitmentSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.campus_recruitment_system;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CampusRecruitmentSystemApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
